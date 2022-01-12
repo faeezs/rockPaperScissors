38ed1397d5efd5be6d762aba7e9427e7726cc293
faeezs <faeezs@gmail.com>
2022-01-12 16:12:02 +0700

    Added docs to fuctions

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/**
+ * @name playRound
+ * @description Plays a round of Rock, Paper, Scissors against the computer
+ * @param {number} playerSelection 
+ * @param {number} computerSelection 
+ * @param {object} outcomeText 
+ * 
+ * @returns {object} result
+ */
 function playRound(playerSelection, computerSelection, outcomeText) {
     let battleResult = playerSelection - computerSelection;
-    let result= new Object();
-    
+    let result = new Object();
+
     switch (battleResult) {
         case 0:
             result.message = `Tie! You Both Chose ${capitalizeFirstLetter(outcomeText[playerSelection].choice)}`
@@ -24,6 +33,17 @@ function playRound(playerSelection, computerSelection, outcomeText) {
     return result;
 }
 
+/**
+ * @name capitalizeFirstLetter
+ * @description Capitalizes the first letter of a  word
+ * @param {string} string
+ * 
+ * @returns {string} result
+ */
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+
 const outcome = ['You Lose!', 'You Win'];
 
 const outcomeText = [
@@ -32,29 +52,29 @@ const outcomeText = [
     { choice: "scissors", loss: "gets Smashed by", win: "Cuts" },
 ];
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-}
-
+// Initialise plaerInput
 let playerInput = "waiting";
-while (playerInput || playerInput === true ) {
-    let score = {player:0, computer:0};
-        for (let i = 0; i < 5; i++) {
-            const computerSelection = () => Math.floor(Math.random() * 3);            // console.log(computerSelection());
-            playerInput = prompt("What's your sign?");
-            console.log(playerInput);
-            if (playerInput === null) {
-                alert("Leaving so soon?")
-                break;
-            }
-            const playerSelection = 0;
-            const fight = playRound(playerSelection, computerSelection(),outcomeText);
-            score.player += fight.playerScore;
-            score.computer += fight.computerScore;
-            console.log(`Round ${i + 1} reults:`)
-            console.log(fight.message);
-            console.log(score);
+
+// Run while the player choses to play and doesn't click cancel
+while (playerInput || playerInput === true) {
+    // Initialize score
+    let score = { player: 0, computer: 0 };
+    // Play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = () => Math.floor(Math.random() * 3);            // console.log(computerSelection());
+        playerInput = prompt("What's your sign?");
+        console.log(playerInput);
+        if (playerInput === null) {
+            alert("Rock, Paper or Scissors?")
+            break;
         }
+        const playerSelection = 0;
+        const fight = playRound(playerSelection, computerSelection(), outcomeText);
+        score.player += fight.playerScore;
+        score.computer += fight.computerScore;
+        console.log(`Round ${i + 1} reults:`)
+        console.log(fight.message);
+        console.log(score);
+    }
     playerInput = confirm("Let's Play Again!");
-    
 }
